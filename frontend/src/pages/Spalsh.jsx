ea828de15e5701331fc8ba58417ea6e6460c97ba
Chenoa <chenoa.perkett@gmail.com>
2024-10-03 00:26:50 +0200

    create splash page

--- frontend/src/pages/Spalsh.jsx
+++ frontend/src/pages/Spalsh.jsx
@@ -3,9 +3,23 @@ import React, { Component } from 'react';
 class Spalsh extends Component {
   render() {
     return (
-      <div className="flex flex-col items-center justify-center h-screen bg-blue-500 text-white">
-        <h1 className="text-5xl font-bold">Welcome to the Home Page</h1>
-        <p className="mt-4 text-lg">This page has its own styling!</p>
+      <div className="p-4 h-screen bg-blue-900 text-white">
+        <div className='flex'>
+          <img className="mr-4 h-16 w-16" src="/assets/images/logo.png" alt="rewind logo" />
+          <h1 className='text-5xl font-bold'>Rewind</h1>
+        </div>
+
+        <main className="flex flex-col items-center h-full justify-center">
+          <h1 className="text-5xl font-bold">Welcome to Rewind</h1>
+          <p className="mt-4 text-2xl text-center max-w-lg">Your personal music hub, where you can relive memories through the power of music.</p>
+
+          <div className='flex my-4'>
+            <button className="p-2 mr-4 bg-blue-300 font-bold text-blue-900 rounded-2xl">Get Started</button>
+            <button className="p-2 bg-white font-bold text-blue-900 rounded-2xl">Login</button>
+          </div>
+
+          <p className="text-xl">Your Playlist, Your Memories</p>
+        </main>
       </div>
     );
   }
