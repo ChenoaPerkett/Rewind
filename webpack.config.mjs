2bcf7e25252f52986bd7b389fb4732177c289992
Chenoa <chenoa.perkett@gmail.com>
2024-10-02 22:15:24 +0200

    setup folder structure

---
+++ webpack.config.mjs
@@ -0,0 +1,45 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: './frontend/src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'frontend', 'public'),
+        filename: 'bundle.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/, 
+                use: ['style-loader', 'css-loader'], 
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]', 
+                            outputPath: 'assets/images', 
+                            publicPath: '/public/assets/images', 
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'], 
+    },
+};
