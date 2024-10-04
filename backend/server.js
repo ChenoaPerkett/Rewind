13e7ff53c41cbfd93096c2aac44f1b35bbbb98f9
Chenoa <chenoa.perkett@gmail.com>
2024-10-04 05:53:11 +0200

    added middleware and updated server file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,25 @@
-import express from "express";
+const express = require('express');
+
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const songRoutes = require('./routes/songRoutes');
+const playlistRoutes = require('./routes/playlistRoutes');
+
+require('dotenv').config();
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static("frontend/public"));
 
-app.listen(3000, () => {
-console.log("Listening on port 3000");
+connectDB();
+
+app.use('/api/users', userRoutes);
+app.use('/api/songs', songRoutes);
+app.use('/api/playlists', playlistRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
